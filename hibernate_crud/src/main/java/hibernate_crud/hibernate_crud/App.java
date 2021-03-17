11ba18313ef125059e761432fe10cb05545e4fba
Naveen <74000045+NaveenMulaga@users.noreply.github.com>
2021-03-17 16:20:31 +0530

    Hibernate CRUD Operations

---
+++ hibernate_crud/src/main/java/hibernate_crud/hibernate_crud/App.java
@@ -0,0 +1,14 @@
+
+package hibernate_crud.hibernate_crud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
